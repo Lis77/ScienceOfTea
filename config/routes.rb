b37cdf33e622f0276b344f3001c1d022cc0ebcda
lis77 <melissadawnmartell@gmail.com>
2015-04-19 21:17:15 +0000

    changed some issues with links in intro. more css

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :advices
   
 
-  root 'order_teas#new'
+  root 'pages#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
